26def841a894514461b1b2f9b9cb9b8ab3701f57
Debasish-selise <debasish.saha@selise.ch>
2023-02-06 22:19:46 +0600

    section two

---
+++ webApi/webApi/Data/WebApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApi.Models.Domain;
+
+namespace webApi.Data
+{
+    public class WebApiDbContext: DbContext
+    {
+        public WebApiDbContext(DbContextOptions<WebApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
